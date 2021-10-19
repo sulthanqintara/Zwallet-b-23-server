46212bb470f6aafb1a0dfc8d5c693811e2a5f8db
Candra Sidik Dermawan <projectcsd07@gmail.com>
2021-10-20 05:23:32 +0700

    add connection socket io and file README.md

--- index.js
+++ index.js
@@ -4,24 +4,32 @@ const logger = require("morgan");
 const mainRouter = require("./src/routes/index");
 const cors = require("cors");
 const app = express();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["https://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 const port = process.env.SECRET_PORT;
 
 // instalasi parser
 app.use(express.urlencoded({ extended: false })); // memasang middleware parsing url-encoded
 app.use(express.json()); // memasang middleware parsing raw json
 app.use(logger("dev"));
-
-
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Method", "GET,POST,PATCH,DELETE");
-//   res.setHeader("Access-Control-Allow-Header", "Content-Type, Authorization, x-access-token");
-//   next();
-// });
 app.use(express.static("public"));
 app.use(mainRouter);
+io.on("connection", (socket) => {
+  console.log("Socket Connected on", socket.id);
+});
 
 // BASE URL => http://localhost:8000
-app.listen(port, () => console.log(`App started running at ${port}`));
-
+httpServer.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
+const socketIoObject = io;
+module.exports.ioObject = socketIoObject;
